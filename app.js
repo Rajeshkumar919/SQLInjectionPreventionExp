09e75237b0ed30d2da3cc91e3a793086f2d48778
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-12-06 18:50:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// App.js
+
+import React from 'react';
+import Login from './Login';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your App Title</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
 
