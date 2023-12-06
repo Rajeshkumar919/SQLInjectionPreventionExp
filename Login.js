80796961f3f48a73e1d33df7695339fac974691a
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-12-06 18:52:22 +0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,1 +1,47 @@
+// Login.js
+
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // You can send a request to the server to validate the login credentials here
+    console.log('Logging in with:', { username, password });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
 
