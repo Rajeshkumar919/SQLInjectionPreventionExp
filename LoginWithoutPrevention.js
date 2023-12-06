61504b376f1b06c7879ccd26c878e9b71779c320
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-12-06 18:54:23 +0500

    Create LoginWithoutPrevention.js

---
+++ LoginWithoutPrevention.js
@@ -0,0 +1,60 @@
+// Login.js
+
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    // Warning: This example is susceptible to SQL injection, do not use in production!
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log('Login successful:', data);
+    } else {
+      console.error('Login failed:', data);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
